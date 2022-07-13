74fe029e55c7ceb52e0f467822642c9a14757ab5
Thomas Auinger <thomas@auinger.de>
2022-07-13 12:24:22 +0200

    completed 10-orderservice as config-client

---
+++ 10-orderservice/src/main/java/training/springcloud/order/OrderRestController.java
@@ -0,0 +1,17 @@
+package training.springcloud.order;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class OrderRestController {
+
+    @Value("${greeting.message}")
+    private String greetingMessage;
+
+    @GetMapping("/greeting")
+    public String getGreeting() {
+        return greetingMessage;
+    }
+}
