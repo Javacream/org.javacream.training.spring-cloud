9676cbeee9c7fb035127bee72b57a5a0a6b43233
Thomas Auinger <thomas@auinger.de>
2022-07-20 15:58:30 +0200

    added customservice call to orderservice, added fallback

--- 10-orderservice/src/main/java/training/springcloud/order/OrderService.java
+++ 10-orderservice/src/main/java/training/springcloud/order/OrderService.java
@@ -1,19 +1,15 @@
 package training.springcloud.order;
 
-import io.github.resilience4j.bulkhead.annotation.Bulkhead;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.cloud.sleuth.Span;
 import org.springframework.cloud.sleuth.Tracer;
 import org.springframework.cloud.sleuth.annotation.NewSpan;
 import org.springframework.core.annotation.Order;
 import org.springframework.stereotype.Service;
-import training.springcloud.order.client.product.Product;
+import training.springcloud.order.client.customer.CustomerClient;
 import training.springcloud.order.client.product.ProductClient;
 
-import java.util.List;
 import java.util.Map;
-import java.util.concurrent.CompletableFuture;
 import java.util.stream.Collectors;
 
 @Service
@@ -23,12 +19,15 @@ public class OrderService {
 
     private final ProductClient productClient;
 
+    private final CustomerClient customerClient;
+
     private final Tracer tracer;
 
     @NewSpan
     public Order placeOrder(String mobilePhoneNumber, Map<String, Integer> productQuantities) {
         log.info("placeOrder({}, {})", mobilePhoneNumber, productQuantities);
 
+        // products
         var currentSpan = tracer.currentSpan();
         var products = productQuantities.keySet().parallelStream()
                 .map(
@@ -38,6 +37,10 @@ public class OrderService {
                         })
                 .collect(Collectors.toList());
         log.info("Collected {} product(s)", products.size());
+
+        // customer
+        var customer = customerClient.getCustomers(mobilePhoneNumber);
+        log.info("Got customer with name '{}'", customer.getName());
 
         return null;
     }
